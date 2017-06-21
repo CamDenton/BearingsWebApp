dc99b726414a396fcaee89953c0d7370a7e440ee
CamDenton <cdentonPD@gmail.com>
2017-06-20 20:06:10 -0400

    Login and Register Redirects

--- BearingsWebApp/Controllers/AccountController.cs
+++ BearingsWebApp/Controllers/AccountController.cs
@@ -79,7 +79,7 @@ namespace BearingsWebApp.Controllers
             switch (result)
             {
                 case SignInStatus.Success:
-                    return RedirectToLocal(returnUrl);
+                    return RedirectToAction("Index", "MeebaInfoes");
                 case SignInStatus.LockedOut:
                     return View("Lockout");
                 case SignInStatus.RequiresVerification:
@@ -163,7 +163,7 @@ namespace BearingsWebApp.Controllers
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
 
-                    return RedirectToAction("Index", "Home");
+                    return RedirectToAction("Index", "MeebaInfoes");
                 }
                 AddErrors(result);
             }
@@ -333,7 +333,7 @@ namespace BearingsWebApp.Controllers
             switch (result)
             {
                 case SignInStatus.Success:
-                    return RedirectToLocal(returnUrl);
+                    return RedirectToAction("Index", "MeebaInfoes");
                 case SignInStatus.LockedOut:
                     return View("Lockout");
                 case SignInStatus.RequiresVerification:
